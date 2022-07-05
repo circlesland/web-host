2dbd11fbaf24c2c4dd5b99df124ac89ae6f5ac09
Razvan Statescu <email@example.com>
2022-07-05 15:42:37 +0300

    implement basic auth flow

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,58 @@
 import App from 'frame-app/src/App.svelte';
 
-import type { ICirclesCustomWindow } from "frame-app/types/types";
+import { AUTH_API_URL } from '../config/api';
 
-declare var window: ICirclesCustomWindow;
+import type { ICirclesCustomWindow } from "frame-app/types/types";
+declare global {
+  interface Window extends ICirclesCustomWindow {}
+}
 
 window.authApi = {
-  login: () => alert("login called from web"),
-  logout: () => alert("logout called from web"),
+  login: () => {
+    window.location.assign(AUTH_API_URL);
+  },
+  logout: () => {
+    localStorage.removeItem("privateKey");
+    localStorage.removeItem("openlogin_store");
+  },
+  processAuth: (window) => {
+    const _url = new URL(window.location.toString());
+    const userDataParam = _url.searchParams.get("user_data");
+
+    try {
+      if (userDataParam) {
+        const userDataJson = JSON.parse(atob(userDataParam));
+        if (userDataJson) {
+          Object.keys(userDataJson).forEach(function (k) {
+            localStorage.setItem(k, userDataJson[k]);
+          });
+        }
+
+        _url.searchParams.delete("user_data");
+        window.location.assign(_url.toString());
+
+        return true;
+      }
+    } finally {
+      return false;
+    }
+  },
+  getDataFromLocalStorage: () => {
+    const privateKey = localStorage.getItem("privateKey");
+    const _accountData = localStorage.getItem("openlogin_store");
+    if (privateKey && _accountData) {
+      const accountData = JSON.parse(_accountData);
+      return {
+        sessionId: accountData.sessionId,
+        email: accountData.email,
+        name: accountData.name,
+        profileImage: accountData.profileImage,
+        privateKey,
+      };
+    }
+
+    return null;
+  },
 };
 
 const app = new App({
